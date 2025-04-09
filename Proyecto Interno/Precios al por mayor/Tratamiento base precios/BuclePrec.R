2b28de472fce04c4e29bde4b7871e17cc4c15bb5
danielavaldesc <danielavaldesc@javerianacali.edu.co>
2025-04-09 10:17:21 -0500

    base
    act

---
+++ Proyecto Interno/Precios al por mayor/Tratamiento base precios/BuclePrec.R
@@ -0,0 +1,32 @@
+# Librería
+library(dplyr)
+
+# Definición de data.frame para las fechas
+fechas <- data.frame(
+  month = rep(1:12, 7),
+  year = rep(2018:2024, each = 12) 
+)
+
+# Obtener los años únicos
+years <- unique(fechas$year)
+
+# Bucle por año
+for (y in years) {
+  df_year <- data.frame()  # DataFrame vacío para cada año
+  
+  
+  for (m in 1:12) {
+    print(paste0("Done ", m, "-", y))
+    
+    df.aux <- as.data.frame(DataCol3(Month = m, Year = y)) %>%
+      mutate(Month = m, Year = y)  
+    
+    df_year <- bind_rows(df_year, df.aux)  
+  }
+  
+  # Guardar el archivo 
+  saveRDS(df_year, file = file.path("C:/Users/danie/OneDrive/Documentos", paste0(y, ".rds")))
+  
+  
+  
+}
