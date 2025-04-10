aa10733690d5956fae8206eeca45247fda4fa153
sergiobarona03 <115027994+sergiobarona03@users.noreply.github.com>
2025-04-10 16:45:43 -0500

    Lista de alimentos 2018-2023 y 2018-2024

---
+++ Proyecto Interno/Precios al por mayor/Mapeo SIPSA-TCAC/mapeo_tcac.R
@@ -0,0 +1,71 @@
+
+##------------------------------------------##
+## Mapeo: SIPSA (precios mayoristas) - TCAC ##
+##------------------------------------------##
+
+# Cargar paquetes
+library(tidyverse)
+
+# Directorio
+setwd("C:\\Users\\Portatil\\Desktop\\Least-cost-diets-and-affordability\\Proyecto Interno\\Precios al por mayor\\")
+
+##---------------------------------##
+## 1. Mapeo SIPSA-TCAC 2018 - 2023 ##
+##---------------------------------##
+
+# Cargar datos sobre precios mayoristas (SIPSA)
+input <- vector(mode = "list", length = 6)
+year <- 2018:2023
+  
+for (k in 1:length(year)) {
+  print(paste0("Done: ", year[k]))
+  input[[k]] <- readRDS(paste0("Bases historicas\\",year[k],".rds"))
+}
+
+overall_input <- do.call(rbind, input) %>% select(Fecha, Year, Month,
+                                                  Grupo, Alimento, Mercado, Precio_kg)
+
+# Eliminar duplicados
+list_input <- data.frame(Alimento = levels(as.factor(overall_input$Alimento)))
+
+# Extrar la base de datos TCAC desde el paqueute FoodpriceR
+library(FoodpriceR)
+tcac <- FoodpriceR::Mapeo_Sipsa_TCAC
+
+# Mapeo simple
+overall_tcac_sipsa <- merge(list_input, tcac, by = "Alimento", all.x = TRUE)
+
+# Guardar 
+writexl::write_xlsx(overall_tcac_sipsa, "Mapeo SIPSA-TCAC\\1823_mapeo_sipsa_tcac.xlsx")
+
+
+##---------------------------------##
+## 1. Mapeo SIPSA-TCAC 2018 - 2024 ##
+##---------------------------------##
+
+rm(list = ls())
+
+# Cargar datos sobre precios mayoristas (SIPSA)
+input <- vector(mode = "list", length = 7)
+year <- 2018:2024
+
+for (k in 1:length(year)) {
+  print(paste0("Done: ", year[k]))
+  input[[k]] <- readRDS(paste0("Bases historicas\\",year[k],".rds"))
+}
+
+overall_input <- do.call(rbind, input) %>% select(Fecha, Year, Month,
+                                                  Grupo, Alimento, Mercado, Precio_kg)
+
+# Eliminar duplicados
+list_input <- data.frame(Alimento = levels(as.factor(overall_input$Alimento)))
+
+# Extrar la base de datos TCAC desde el paqueute FoodpriceR
+library(FoodpriceR)
+tcac <- FoodpriceR::Mapeo_Sipsa_TCAC
+
+# Mapeo simple
+overall_tcac_sipsa <- merge(list_input, tcac, by = "Alimento", all.x = TRUE)
+
+# Guardar 
+writexl::write_xlsx(overall_tcac_sipsa, "Mapeo SIPSA-TCAC\\1824_mapeo_sipsa_tcac.xlsx")
