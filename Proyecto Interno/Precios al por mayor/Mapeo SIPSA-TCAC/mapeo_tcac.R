4786a75bf656a2f3baea707a26b5f7bf4ce72b32
sergiobarona03 <115027994+sergiobarona03@users.noreply.github.com>
2025-04-25 11:55:58 -0500

    Mapeo TCAC-SIPSA incluyendo la variable de municipio

--- Proyecto Interno/Precios al por mayor/Mapeo SIPSA-TCAC/mapeo_tcac.R
+++ Proyecto Interno/Precios al por mayor/Mapeo SIPSA-TCAC/mapeo_tcac.R
@@ -69,3 +69,47 @@ overall_tcac_sipsa <- merge(list_input, tcac, by = "Alimento", all.x = TRUE)
 
 # Guardar 
 writexl::write_xlsx(overall_tcac_sipsa, "Mapeo SIPSA-TCAC\\1824_mapeo_sipsa_tcac.xlsx")
+
+
+##-----------------------------------##
+## 2. Mapeo SIPSA-TCAC 2018 - 2024:  ##
+##-----------------------------------##
+
+rm(list = ls())
+
+# Cargar datos sobre precios mayoristas (SIPSA)
+input <- vector(mode = "list", length = 7)
+year <- 2018:2024
+
+for (k in 1:length(year)) {
+  print(paste0("Done: ", year[k]))
+  input[[k]] <- readRDS(paste0("Bases historicas\\",year[k],".rds"))
+}
+
+overall_input <- do.call(rbind, input) %>% select(Fecha, Year, Month,
+                                                  Grupo, Alimento, Mercado)
+
+# Crear la variable municipio
+overall_input <- overall_input %>%
+  mutate(
+    Municipio = str_trim(str_extract(Mercado, "^[^(,]+"))
+  ) %>% distinct()
+
+# Extrar la base de datos TCAC desde el paqueute FoodpriceR
+library(FoodpriceR)
+tcac <- FoodpriceR::Mapeo_Sipsa_TCAC
+
+# Mapeo simple
+overall_tcac_sipsa <- merge(overall_input, tcac, by = "Alimento", all.x = TRUE)
+
+overall_tcac_sipsa <- overall_tcac_sipsa %>% janitor::clean_names()
+
+
+# Guardar 
+writexl::write_xlsx(overall_tcac_sipsa, "Mapeo SIPSA-TCAC\\1824_mun_mapeo_sipsa_tcac.xlsx")
+
+
+
+
+
+
